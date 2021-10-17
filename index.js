59fa5f6342b5b6bd0dc0901b08c1364fe9541439
takinaga-dev <takuya_inagaki@yahoo.ne.jp>
2021-10-17 19:46:17 +0900

    get repo list

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-
+const { Octokit } = require("@octokit/rest")
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
@@ -11,8 +11,18 @@ try {
   core.setOutput("test", "俺の叫びを聞いてくれ");
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // console.log(`The event payload: ${payload}`);
   core.warning('refer to http://yahoo.co.jp');
+
+  const octokit = new Octokit();
+
+  octokit.rest.repos.listForOrg({
+    org: "Jij-Inc",
+    type: "public"
+  }).then(({data}) => {
+    console.log(data);
+  })
 } catch (error) {
   core.setFailed(error.message);
 }
+
